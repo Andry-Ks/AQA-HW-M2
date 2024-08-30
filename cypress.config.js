3da171f96b4cc2063aeba388785b048a607f6e5b
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2024-08-31 01:33:49 +0300

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,11 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      user1: process.env.CYPRESS_USER1,
+      password1: process.env.CYPRESS_PASSWORD1,
+      user2: process.env.CYPRESS_USER2,
+      password2: process.env.CYPRESS_PASSWORD2,
+    },
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,41 +1,26 @@
-//npx cypress open
+"use strict";
+const user1 = Cypress.env('user1');
+const password1 = Cypress.env('password1');
+const user2 = Cypress.env('user2');
+const password2 = Cypress.env('password2');
 
-// Тест №1
 describe("Login and Logout Test 1", () => {
   it("should login and logout for user 1", () => {
-    // Відкрити сторінку
     cy.visit("https://www.edu.goit.global/account/login");
-
-    // Використовуємо команду login для входу
-    cy.login("user888@gmail.com", "1234567890");
-
-    // Знаходимо та клікаємо на кнопку для відкриття меню сайту
+    cy.login(user1, password1);
     cy.get("#open-navigation-menu-mobile").click();
-
-    // Знаходимо кнопку "Log out" в меню та клікаємо на неї
     cy.contains("Log out").click();
-
     // Перевіряємо, що повернулися на сторінку
     cy.url().should("eq", "https://www.edu.goit.global/account/login");
   });
 });
 
-// Тест №2
 describe("Login and Logout Test 2", () => {
   it("should login and logout for user 2", () => {
-    // Відкрити сторінку
     cy.visit("https://www.edu.goit.global/account/login");
-
-    // Використовуємо команду login для входу
-    cy.login("testowyqa@qa.team", "QA!automation-1");
-
-    // Знаходимо та клікаємо на кнопку для відкриття меню сайту
+    cy.login(user2, password2);
     cy.get("#open-navigation-menu-mobile").click();
-
-    // Знаходимо кнопку "Log out" в меню та клікаємо на неї
     cy.contains("Log out").click();
-
-    // Перевіряємо, що повернулися на сторінку
     cy.url().should("eq", "https://www.edu.goit.global/account/login");
   });
 });
